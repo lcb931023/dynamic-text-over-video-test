76f86ddd60de688899d0c6781c73e55dfb6ca294
Changbai Li <changbai.li@heliosinteractive.com>
2016-07-22 17:33:09 -0700

    clear out text before animating

--- text-demo/main.js
+++ text-demo/main.js
@@ -11,6 +11,17 @@ const options = {
   characters: characters
 };
 
+const optionsOther = {
+  // Initial position
+  offset: 0,
+  // Timeout between each random character
+  timeout: 23,
+  // Number of random characters to show
+  iterations: 4,
+  // Random characters to pick from
+  characters: characters
+};
+
 // Callback function when resolve completes
 function callback(options) {
   console.log("String animation complete", options);
@@ -18,26 +29,40 @@ function callback(options) {
 
 
 var animate = function () {
-  resolver.resolve(Object.assign({}, options, {element:document.querySelector('.date')}), aniLocation);
-  function aniLocation() {
-    resolver.resolve(Object.assign({}, options, {element:document.querySelector('.location')}), aniOperativeTitle);
-  }
-  function aniOperativeTitle() {
-    resolver.resolve(Object.assign({}, options, {element:document.querySelector('.operative-title')}), aniOperativeName);
-  }
-  function aniOperativeName() {
-    resolver.resolve(Object.assign({}, options, {element:document.querySelector('.operative-name')}), aniOperativeArea);
-  }
-  function aniOperativeArea() {
-    resolver.resolve(Object.assign({}, options, {element:document.querySelector('.operative-area')}), aniOrigin);
-  }
-  function aniOrigin() {
-    resolver.resolve(Object.assign({}, options, {element:document.querySelector('.origin')}), aniClearance);
-  }
-  function aniClearance() {
-    resolver.resolve(Object.assign({}, options, {element:document.querySelector('.clearance')}), callback);
-  }
+  document.querySelectorAll('.animated-text').forEach(function(element){
+    element.textContent = '';
+  });
+  resolver.resolve(Object.assign({}, options, {element:document.querySelector('.date')}), callback);
+  resolver.resolve(Object.assign({}, options, {element:document.querySelector('.location')}), callback);
+  resolver.resolve(Object.assign({}, options, {element:document.querySelector('.operative-title')}), callback);
+  setTimeout(function(){
+    resolver.resolve(Object.assign({}, optionsOther, {element:document.querySelector('.operative-name')}), callback);
+  }, 2500);
+  resolver.resolve(Object.assign({}, options, {element:document.querySelector('.operative-area')}), callback);
+  resolver.resolve(Object.assign({}, options, {element:document.querySelector('.origin')}), callback);
+  resolver.resolve(Object.assign({}, options, {element:document.querySelector('.clearance')}), callback);
 }
+// var animate = function () {
+//   resolver.resolve(Object.assign({}, options, {element:document.querySelector('.date')}), aniLocation);
+//   function aniLocation() {
+//     resolver.resolve(Object.assign({}, options, {element:document.querySelector('.location')}), aniOperativeTitle);
+//   }
+//   function aniOperativeTitle() {
+//     resolver.resolve(Object.assign({}, options, {element:document.querySelector('.operative-title')}), aniOperativeName);
+//   }
+//   function aniOperativeName() {
+//     resolver.resolve(Object.assign({}, options, {element:document.querySelector('.operative-name')}), aniOperativeArea);
+//   }
+//   function aniOperativeArea() {
+//     resolver.resolve(Object.assign({}, options, {element:document.querySelector('.operative-area')}), aniOrigin);
+//   }
+//   function aniOrigin() {
+//     resolver.resolve(Object.assign({}, options, {element:document.querySelector('.origin')}), aniClearance);
+//   }
+//   function aniClearance() {
+//     resolver.resolve(Object.assign({}, options, {element:document.querySelector('.clearance')}), callback);
+//   }
+// }
 
 var gui = new dat.GUI();
 gui.add(options, 'offset', 0);
